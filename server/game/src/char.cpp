1aa4635be3c06bbdc6723b820f6eae531a07d0bf
sobolanescu <38504992+sobolanescu@users.noreply.github.com>
2021-01-12 19:41:50 +0200

    Create char.cpp

---
+++ server/game/src/char.cpp
@@ -0,0 +1,56 @@
+Search:
+
+void CHARACTER::Initialize()
+{
+	CEntity::Initialize(ENTITY_CHARACTER);
+
+	m_bNoOpenedShop = true;
+
+	m_bOpeningSafebox = false;
+
+	m_fSyncTime = get_float_time()-3;
+	m_dwPlayerID = 0;
+	m_dwKillerPID = 0;
+
+
+Add:
+
+#ifdef ENABLE_ANTI_HEADER_FLOOD
+	analyze_protect = 0;
+	analyze_protect_count = 0;
+#endif
+
+
+
+Search:
+
+void CHARACTER::Destroy()
+{
+	CloseMyShop();
+
+	if (m_pkRegen)
+	{
+		if (m_pkDungeon) {
+			// Dungeon regen may not be valid at this point
+			if (m_pkDungeon->IsValidRegen(m_pkRegen, regen_id_)) {
+				--m_pkRegen->count;
+			}
+		} else {
+			// Is this really safe?
+			--m_pkRegen->count;
+		}
+		m_pkRegen = NULL;
+	}
+
+	if (m_pkDungeon)
+	{
+		SetDungeon(NULL);
+	}
+  
+ 
+Add:
+  
+#ifdef ENABLE_ANTI_HEADER_FLOOD
+	analyze_protect = 0;
+	analyze_protect_count = 0;
+#endif
