2f44e5984ab5ab71f6ccb51f8afc36377af7e486
sobolanescu <38504992+sobolanescu@users.noreply.github.com>
2021-01-12 19:43:39 +0200

    Create input_main.cpp

---
+++ server/game/src/input_main.cpp
@@ -0,0 +1,37 @@
+Search:
+
+int CInputMain::Analyze(LPDESC d, BYTE bHeader, const char * c_pData)
+{
+	LPCHARACTER ch;
+
+	if (!(ch = d->GetCharacter()))
+	{
+		sys_err("no character on desc");
+		d->SetPhase(PHASE_CLOSE);
+		return (0);
+	}
+  
+
+Add:
+
+#ifdef ENABLE_ANTI_HEADER_FLOOD
+	if (ch && ch->IsPC())
+	{
+		if (get_global_time() < ch->analyze_protect)
+		{
+			ch->analyze_protect_count = ch->analyze_protect_count + 1;
+			
+			if (ch->analyze_protect_count >= 300)
+			{
+				sys_err("HEADER_FLOOD_DETECTED: Name: %s Count: %d", ch->GetName(), ch->analyze_protect_count);
+				ch->analyze_protect_count = 0;
+				d->SetPhase(PHASE_CLOSE);
+				return (0);
+			}
+		}
+		else
+			ch->analyze_protect_count = 0;
+		
+		ch->analyze_protect = get_global_time() + 1;
+	}
+#endif
