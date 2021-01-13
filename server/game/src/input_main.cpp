18e7869615a613cd4e5de8f6cd2b4a03c2d584db
sobolanescu <38504992+sobolanescu@users.noreply.github.com>
2021-01-13 11:23:23 +0200

    Update input_main.cpp

--- server/game/src/input_main.cpp
+++ server/game/src/input_main.cpp
@@ -15,13 +15,13 @@ int CInputMain::Analyze(LPDESC d, BYTE bHeader, const char * c_pData)
 Add:
 
 #ifdef ENABLE_ANTI_HEADER_FLOOD
-	if (ch && ch->IsPC() && bHeader != HEADER_CG_ITEM_PICKUP)
+	if (ch && ch->IsPC())
 	{
 		if (get_global_time() < ch->analyze_protect )
 		{
 			ch->analyze_protect_count = ch->analyze_protect_count + 1;
 			
-			if (ch->analyze_protect_count >= 300)
+			if (ch->analyze_protect_count >= 500)
 			{
 				sys_err("HEADER_FLOOD_DETECTED: Name: %s Count: %d Header: %d", ch->GetName(), ch->analyze_protect_count, bHeader);
 				ch->analyze_protect_count = 0;
